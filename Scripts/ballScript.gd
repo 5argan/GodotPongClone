d35186ad239d6747481c09bcf26f2fa9daacbb21
5argan <tizian.hoffmann@gmail.com>
2017-06-12 17:23:02 +0200

    Scoring a goal now increments the according player's score and resets the ball.

--- Scripts/ballScript.gd
+++ Scripts/ballScript.gd
@@ -1,12 +1,14 @@
 extends KinematicBody2D
 
-const velocity = Vector2(-100, 0)
-const speed = 200
+var velocity
+const SPEED = 200
 const MAXBOUNCEANGLE = 75
 var direction = 1
 
 func _ready():
+	randomize()
 	set_fixed_process(true)
+	reset()
 
 func _fixed_process(delta):
 	var remaining_movement = move(velocity*delta)
@@ -30,8 +32,27 @@ func get_bounce_angle(my_pos_y, target_pos_y, target_size_y):
 	
 func bounce_off_bat(bounce_angle):
 	direction = direction * -1
-	velocity.x = direction * speed * cos(bounce_angle)
-	velocity.y = speed * sin(bounce_angle)
+	velocity.x = direction * SPEED * cos(bounce_angle)
+	velocity.y = SPEED * sin(bounce_angle)
 
 func bounce_off_wall():
 	velocity.y = -velocity.y
+
+func _on_Player1Goal_body_enter( body ):
+	if body == self:
+		var player2 = get_node("../EnemyBat")
+		player2.score += 1
+		reset()
+
+func _on_Player2Goal_body_enter( body ):
+	if body == self:
+		var player1 = get_node("../PlayerBat")
+		player1.score += 1
+		reset()
+
+func reset():
+	set_pos(Vector2(512, 300))
+	direction = randi() % 2 * 2 - 1
+	var angle = rand_range(0, MAXBOUNCEANGLE) / 360 * 2 * PI
+	var rotation_vector = Vector2(cos(angle), sin(angle))
+	velocity = direction * SPEED * rotation_vector
